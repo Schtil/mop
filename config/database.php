60aa1e3214da0719ff8ce4b0e128adea41c871d5
Schtil <schtil@schtil.com>
2021-02-11 18:49:33 +0300

    fix october env file generated

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'port' => env('DB_PORT', 3306),
             'database' => env('DB_DATABASE', 'october'),
             'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),jhpjdf',
+            'password' => env('DB_PASSWORD', ''),
             'charset'    => 'utf8mb4',
             'collation'  => 'utf8mb4_unicode_ci',
             'prefix'     => '',
